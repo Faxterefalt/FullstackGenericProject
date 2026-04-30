ee3dae1c55e419baa1a9717d073f1a93d5b82096
Faxter <fabianazuga123hotmail.com>
2026-04-30 19:23:59 -0400

    blade format controller

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,16 +6,11 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PasswordController;
-
-//blog.test => welcome
-//blog.test/contacto => contact
-//blog.test/blog => blog
-//blog.test/nosotros => about
-
+use App\Http\Controllers\PostController;
 
 Route::view('/','welcome')->name('home');
 Route::view('contacto','contact')->name('contact');
-Route::view('blog','blog')->name('blog');
+Route::get('blog', [PostController::class, 'index'])->name('blog');
 Route::view('nosotros','about')->name('about');
 
 
