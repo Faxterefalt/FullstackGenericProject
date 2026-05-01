9d7f629e5bdea9eef4ffa688d5b6355afbc1553d
Faxter <fabianazuga123hotmail.com>
2026-04-30 20:11:29 -0400

    db controller

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -3,16 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
+
 {
     public function index(){
-        $posts=[
-            ['title'=>'First Post'],
-            ['title'=>'Second Post'],
-            ['title'=>'Third Post'],
-            ['title'=>'Fourth Post'],
-        ];
+        $posts= DB::table('posts')->get(); 
         return view('blog',['posts'=>$posts]);
     }
 }
