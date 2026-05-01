526c090c838c8aeac604792d894ba14bb4880bb5
Faxterefalt <azugafabian321@gmail.com>
2026-05-01 17:19:12 -0400

    fix: nav estructure

---
+++ blog/resources/views/posts/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout meta-title="Blog" meta-description="Blog Description" >
+    <h1>Blog</h1>    
+    @foreach ($posts as $post)
+        <h2>
+            <a href="{{route('posts.show',$post)}}">
+                {{$post->title}}
+            </a>
+        </h2>
+    @endforeach
+</x-layout>
