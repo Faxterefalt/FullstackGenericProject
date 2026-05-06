f4706f80d3b6fa445fc4f6f9a7f1355e967e0fb8
Faxterefalt <azugafabian321@gmail.com>
2026-05-06 14:49:53 -0400

    DRY method for form fields

--- blog/resources/views/posts/create.blade.php
+++ blog/resources/views/posts/create.blade.php
@@ -9,27 +9,8 @@
 
 <form action="{{route('posts.store')}}" method="POST">
     @csrf
-    <label>
-        {{ __('Title') }}<br/>
-        <input name="title" type="text" value="{{ old('title') }}"/>
-
-        @error('title')
-        <br />
-        <small style="color:red">{{ $message }}</small>
-        @enderror
-
-    </label><br/>
-        <label>
-        {{__('Body')}} <br/>
-        <textarea name="body" >{{old('body')}}</textarea>
-
-        @error('body')
-        <br />
-        <small style="color:red">{{ $message }}</small>
-        @enderror
-
-    </label><br/>
-<button type="submit">{{__('Send')}}</button>
+    @include('posts.form-fields')
+    <button type="submit">{{__('Send')}}</button>
 </form>
 
 <a href="{{route('posts.index')}}">{{__('Back')}}</a>
--- blog/resources/views/posts/edit.blade.php
+++ blog/resources/views/posts/edit.blade.php
@@ -4,27 +4,8 @@
 </h1>
 <form action="{{route('posts.update',$post)}}" method="POST">
     @csrf @method('PATCH')
-    <label>
-        {{ __('Title') }}<br/>
-        <input name="title" type="text" value="{{ old('title',$post->title) }}"/>
-
-        @error('title')
-        <br />
-        <small style="color:red">{{ $message }}</small>
-        @enderror
-
-    </label><br/>
-        <label>
-        {{__('Body')}} <br/>
-        <textarea name="body" >{{old('body',$post->body)}}</textarea>
-
-        @error('body')
-        <br />
-        <small style="color:red">{{ $message }}</small>
-        @enderror
-
-    </label><br/>
-<button type="submit">{{__('Send')}}</button>
+    @include('posts.form-fields')
+    <button type="submit">{{__('Send')}}</button>
 </form>
 <a href="{{route('posts.index')}}">
     Back
