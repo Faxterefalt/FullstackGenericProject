c764b5de48c112c03ab2d0107001e6a5cb7c66aa
Faxterefalt <azugafabian321@gmail.com>
2026-05-15 18:32:21 -0400

    vite config for the first time

--- blog/resources/views/components/layout.blade.php
+++ blog/resources/views/components/layout.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $metaTitle ?? 'Default Title' }}</title>
     <meta name="description" content="{{ $metaDescription ?? 'Default Description'}}"/>
-</head>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+
 <body>
 <x-navigation/>
 
